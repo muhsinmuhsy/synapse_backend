fce516d55b90491d59d7070e0c95eadc1e58f706
muhsinmuhsy <muhsinkoodali856@gmail.com>
2025-04-28 23:14:59 +0530

    project created nad apps are added

---
+++ devforge/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DevforgeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'devforge'
---
+++ visionflow/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VisionflowConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'visionflow'
